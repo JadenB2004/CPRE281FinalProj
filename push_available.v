ffb5f6bf6d54a0e466ed2888a0f2ad6ae4a711d1
JadenB2004 <119357530+JadenB2004@users.noreply.github.com>
2023-05-03 18:11:08 -0500

    Add files via upload

--- push_available.v
+++ push_available.v
@@ -1,62 +1,130 @@
-module push_available(push,pop,A0,A1,O,E);
-input push,pop;
-output A0,A1,O,E;
+module push_available(Clk,state,A0,A1,O,U,push,pop);
+input Clk,state;
+output A0,A1,O,U,pop,push;
 
 integer cursor;
 
-reg A0,A1,O,E;
+reg A0,A1,O,U,pop,push;
 initial
 begin
 cursor = 0;
-E = 0;
+pop = 0;
+push = 0;
 O = 0;
+U= 0;
 
 
 
 end
 
-always@(negedge push)
+always@(negedge Clk)
 		
 	begin
 		case(cursor)
 		0:
 			begin
-				cursor = cursor+1;
-				A0 = 0;
-				A1 = 0;
-				E = 1;
+				if(state == 1)
+				begin
+					cursor = cursor+1;
+					A0 = 0;
+					A1 = 0;
+					push = 1;
+					pop = 0;
+					U=0;
+				end
+				else if(state == 0) 
+				begin
+				cursor = cursor;
+				U=1;
+				end
 			end
 		
 		1:
 			begin
-				cursor = cursor+1;
-				A1 = 0;
-				A0 = 1;
-				
-				E = 1;
+				if(state == 1)
+				begin
+					cursor = cursor+1;
+					A0 = 1;
+					A1 = 0;
+					push = 1;
+					pop = 0;
+					
+				end
+				else if(state == 0) 
+				begin
+				cursor = cursor-1;
+					A0 = 0;
+					A1 = 0;
+					push = 0;
+					pop = 1;
+				end
 			end
 		
 		2:
+			
 			begin
-			cursor = cursor+1;
-				A1 = 1;
-				A0 = 0;
+				if(state == 1)
+				begin
+					cursor = cursor+1;
+					A1 = 1;
+					A0 = 0;
 				
-				E = 1;
+					push = 1;
+					pop = 0;
+				end
+				else if(state == 0) 
+				begin
+				cursor = cursor-1;
+					A1 = 0;
+					A0 = 1;
+				
+					push = 0;
+					pop = 1;
+				end
 			end
 		3:
+			
 			begin
-			cursor = cursor+1;
+			if(state == 1)
+			begin
+				cursor = cursor+1;
 				A1 = 1;
 				A0 = 1;
 				
-				E = 1;
+				push = 1;
+				pop = 0;
+			end
+			else if(state == 0) 
+			begin
+			cursor = cursor-1;
+				A1 = 1;
+				A0 = 0;
+				
+				push = 0;
+				pop = 1;
+			end
 			end
 			
 		4:
+			
+				
 			begin
-				O = 1;
+				if(state == 1)
+				begin
+					O = 1;
+				end
+				else if(state == 0) 
+				begin
+				cursor = cursor-1;
+					A1 = 1;
+					A0 = 1;
+				
+					push = 0;
+					pop = 1;
+					O=0;
+				end
 			end
+			
 			
 		endcase
 	end
